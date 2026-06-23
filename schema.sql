cb9956107adcf190cf8f21063b9aebbcfeb915fb
pl8n4 <pablo.lasarte1283@gmail.com>
2026-06-23 16:59:31 -0500

    update schema

--- schema.sql
+++ schema.sql
@@ -15,6 +15,12 @@ create table filing(
     aff10b5One BOOLEAN,
     issuer_cik INT NOT NULL,
     insider_cik INT NOT NULL,
+    is_director BOOLEAN,
+    is_officer BOOLEAN,
+    is_ten_percent_owner BOOLEAN,
+    is_other BOOLEAN,
+    officer_title TEXT,
+    footnotes JSONB,
     CONSTRAINT fk_filing_issuer_cik FOREIGN KEY (issuer_cik) REFERENCES issuer(issuer_cik)
         ON DELETE RESTRICT
         ON UPDATE CASCADE,
@@ -27,9 +33,13 @@ create table transactions(
     transaction_id SERIAL PRIMARY KEY,
     accession_number TEXT NOT NULL,
     transaction_code CHAR(1),
+    security_title TEXT,
     shares NUMERIC(15, 4),
     price NUMERIC(15, 4),
     disposed_code CHAR(1),
+    shares_owned_following NUMERIC(15, 4),
+    direct_or_indirect_ownership CHAR(1),
+    footnote_ids TEXT[],
     CONSTRAINT fk_filing_accession_number
         FOREIGN KEY (accession_number) REFERENCES filing(accession_number)
         ON DELETE RESTRICT
